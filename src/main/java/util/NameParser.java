72fa9b6927fce72755ce928da5dbad0736f97cfb
Donghun <2dh2@naver.com>
2025-08-04 16:47:36 +0900

    chore. 개행 컨벤션 통일

--- src/main/java/util/NameParser.java
+++ src/main/java/util/NameParser.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.List;
 
 public class NameParser {
+
     public static List<String> parse(String nameStr) {
         return Arrays.stream(nameStr.split(",", -1)).toList();
     }
--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -3,6 +3,7 @@ package view;
 import java.util.Scanner;
 
 public class InputView {
+
     Scanner sc = new Scanner(System.in);
 
     public String inputNames() {
