e87b3eae0085be73466ff59e620bbfed5830d3f0
Donghun <2dh2@naver.com>
2025-08-06 15:12:50 +0900

    refactor. 자동차 이름 검증 로직 모델에서 가지도록 수정

--- src/main/java/model/Car.java
+++ src/main/java/model/Car.java
@@ -1,11 +1,15 @@
 package model;
 
+import exception.ErrorMessage;
+
 public class Car {
 
     private final String name;
     private int pos;
 
     public Car(String name) {
+        validateName(name);
+
         this.name = name;
         pos = 0;
     }
@@ -26,5 +30,22 @@ public class Car {
 
     public String getName() {
         return name;
+    }
+
+    private void validateName(String name) {
+        validateEmptyName(name);
+        validateOverLengthName(name);
+    }
+
+    private void validateEmptyName(String name) {
+        if (name == null || name.isBlank()) {
+            throw new IllegalArgumentException(ErrorMessage.EMPTY_CAR_NAME);
+        }
+    }
+
+    private void validateOverLengthName(String name) {
+        if (name.length() > 5) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_CAR_NAME_LENGTH);
+        }
     }
 }
